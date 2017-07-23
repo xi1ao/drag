cfc859454c23b9937371b73b51884c1d7b2cb0c7
xi1ao <731662391@qq.com>
2017-07-23 18:49:18 +0800

    change drag.js from new1

--- drag.js
+++ drag.js
@@ -1,3 +1,16 @@
 /**
  * Created by xiaoduan on 2017/7/23.
  */
+function drag(id){
+    var obj=document.getElementById(id);
+    var disX=0;
+    var disY=0;
+    obj.onmousedown=function(ev){
+        disX=ev.pageX-obj.offsetLeft;
+        disY=ev.pageY-obj.offsetTop;
+        document.onmousemove=function(ev){
+            obj.style.left=ev.clientX-disX+'px';
+            obj.style.top=ev.clientY-disY+'px';
+        }
+    }
+}
