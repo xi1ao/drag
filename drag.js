ef79afe46fb7ecfe0da1401d2bf57ef26ad90df1
xi1ao <731662391@qq.com>
2017-07-23 19:35:47 +0800

    success drag

--- drag.js
+++ drag.js
@@ -11,6 +11,13 @@ function drag(id){
         document.onmousemove=function(ev){
             obj.style.left=ev.pageX-disX+'px';
             obj.style.top=ev.pageY-disY+'px';
-        }
+        };
+        document.onmouseup=function(){
+            document.onmousemove=null;
+            document.onmouseup=null;
+
+        };
+
     }
+    return false;
 }
